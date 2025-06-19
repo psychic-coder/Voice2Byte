e50edbb97260c66545b106f39a1c71a7f94d01a2
Rohit Ganguly <rohitganguly450@gmail.com>
2025-06-19 23:47:46 +0530

    changes

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "45329f96fed5763a"; }
+/******/ 		__webpack_require__.h = function() { return "5222d8e9a152b771"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
